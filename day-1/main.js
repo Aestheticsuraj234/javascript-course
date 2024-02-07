0472efc460177c959435ab201545565b25904b98
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-07 20:21:50 +0530

    Update index.html and main.js files

--- day-1/main.js
+++ day-1/main.js
@@ -1,48 +1,59 @@
-// Output "Hello world into the other file"
-console.log("Hello world into the other file");
+// // Output "Hello world into the other file"
+// console.log("Hello world into the other file");
 
-// Variables
+// // Variables
 
-// Declaration and assignment using var, let, and const
-var name = "John Doe"; // var
-let age = 30; // let
-console.log(age);
-const hasHobbies = true; // const (constant)
+// // Declaration and assignment using var, let, and const
+// var name = "John Doe"; // var
+// let age = 30; // let
+// console.log(age);
+// const hasHobbies = true; // const (constant)
 
-// Primitive Types
+// // Primitive Types
 
-// Number
-let myAge = 20;
+// // Number
+// let myAge = 20;
 
-// String
-let myName = "SURAJ"; // Using double quotes
-let myName2 = 'SURAJ'; // Using single quotes
-let myName3 = `SURAJ`; // Using backticks for template literals
+// // String
+// let myName = "SURAJ"; // Using double quotes
+// let myName2 = 'SURAJ'; // Using single quotes
+// let myName3 = `SURAJ`; // Using backticks for template literals
 
-// Boolean
-let isExist = false;
-let isExist2 = true;
+// // Boolean
+// let isExist = false;
+// let isExist2 = true;
 
-// Null
-let myNull = null;
+// // Null
+// let myNull = null;
 
-// Undefined
-let myUndefined = undefined;
+// // Undefined
+// let myUndefined = undefined;
 
-// Complex Types
+// // Complex Types
 
-// Object
-let myObject = {
-    name: "John",
-    age: 30
-};
+// // Object
+// let myObject = {
+//     name: "John",
+//     age: 30
+// };
 
-// Array
-let myArray = [1, 2, 3, 4, 5];
+// // Array
+// let myArray = [1, 2, 3, 4, 5];
 
-// Functions
+// // Functions
 
-// Function declaration
-function sum() {
-    console.log(1 + 1);
-}
+// // Function declaration
+// function sum() {
+//     console.log(1 + 1);
+// }
+
+
+// *Create a small HTML page with embedded JavaScript that prompts the user for their name and displays a personalized greeting.
+
+
+// let Username = prompt("Enter Your Name");
+
+// if(Username){
+//     alert("Hello " + Username + "!");
+// }
+
