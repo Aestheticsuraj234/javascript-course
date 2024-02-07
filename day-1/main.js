a605b65096fb79309bea79d75ffe1405a4b842cd
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-07 14:28:39 +0530

    Update function to handle edge cases

---
+++ day-1/main.js
@@ -0,0 +1,42 @@
+console.log("Hello world into the other file")
+
+// variables!
+
+// *var, let, const
+
+var name = "John Doe";
+let age = 30
+console.log(age)
+const hasHobbies = true;
+
+
+// 1. primitive types
+
+// *number, string, boolean, null, undefined, symbol
+
+let myAge = 20;
+let myName = "SURAJ";//--1
+let myName2 = 'SURAJ';//--2
+let myName3 = `SURAJ`; //--3
+let isExist = false , isExist2 = true;
+let myNull = null;
+let myUndefined = undefined;
+
+
+// COMPLEX TYPES
+
+// *object, array, function
+
+let myObject = {
+    name: "John",
+    age: 30
+};
+
+let myArray = [1, 2, 3, 4, 5];
+
+
+// FUNCTIONS
+
+function sum() {
+    console.log(1 + 1);
+}
