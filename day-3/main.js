3694a9834129b86bfe97d922bad9cf53148ffeed
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-09 13:06:03 +0530

    Refactor main.js file

---
+++ day-3/main.js
@@ -0,0 +1,44 @@
+// Console Logging and Commenting
+// console.log('Hello, World!')
+
+// Commented out code for demonstration
+
+/* 
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+*/
+
+// For Loop
+for(let i=0; i<=10; i++){
+    console.log(i);
+}
+
+// For...In Loop
+let obj = {
+    name:"John",
+    age:30,
+    city:"New York"
+}
+
+for(let xyz in obj){
+    console.log(xyz);
+}
+
+// For...Of Loop
+let greet = "Good Morning";
+
+for(let value of greet){
+    console.log(value);
+}
+
+// While Loop
+let i = 0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
