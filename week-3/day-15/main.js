90fc6c2cf78762ee19d6f53328060eac0e550f91
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-24 21:06:39 +0530

    Add sync and async code examples

--- week-3/day-15/main.js
+++ week-3/day-15/main.js
@@ -1,3 +1,20 @@
-// *sync and async kya hota hai?
-// *asynx js hai kyaa?
+// *sync and async?
+
+console.log("I am a sync code") 
+setTimeout(()=>{
+    console.log("I am a async code")
+} , 5000)
+console.log("I am a sync code3")
+
+// setInterval
+// setTimeout
+// fetch
+// axios
+// Promise
+// async / await 
+// XMLHttpRequest
+
+
+
+// *asynx js?
 // *js is not async by default
